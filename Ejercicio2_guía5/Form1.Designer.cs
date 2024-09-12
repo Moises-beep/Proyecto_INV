1c0683db716c8aebd5f5c0b29d89ef4cd3ba6bff
mined <mined@MINIOM>
2024-09-12 09:43:11 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2_guía5/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Ejercicio2_guía5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIngresar = new System.Windows.Forms.TextBox();
+            this.txtSumatoria = new System.Windows.Forms.TextBox();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número a ingresar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sumatoria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Promedio";
+            // 
+            // txtIngresar
+            // 
+            this.txtIngresar.Location = new System.Drawing.Point(117, 34);
+            this.txtIngresar.Name = "txtIngresar";
+            this.txtIngresar.Size = new System.Drawing.Size(137, 20);
+            this.txtIngresar.TabIndex = 3;
+            // 
+            // txtSumatoria
+            // 
+            this.txtSumatoria.Location = new System.Drawing.Point(192, 200);
+            this.txtSumatoria.Name = "txtSumatoria";
+            this.txtSumatoria.Size = new System.Drawing.Size(110, 20);
+            this.txtSumatoria.TabIndex = 4;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(192, 238);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(110, 20);
+            this.txtPromedio.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(44, 73);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(160, 73);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.txtIngresar);
+            this.groupBox1.Location = new System.Drawing.Point(67, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 117);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de entrada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 340);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.txtSumatoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIngresar;
+        private System.Windows.Forms.TextBox txtSumatoria;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Ejercicio2_guía5/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2_guía5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int i, numero;
+            double suma = 0, promedio = 0, cantProm = 0;
+            try
+            {
+                //Validación de ingreso de numeros
+                numero = Convert.ToInt32(txtIngresar.Text);
+                while (numero < 2 || numero > 100)
+                {
+                    MessageBox.Show("Ingrese valores entre 2 y 100. Intente nuevamente");
+                    txtIngresar.Text = "";
+                    return;
+                }
+                //Sumatoria impares
+
+                for (i = 2; i <= numero; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        suma = suma + i;
+                    }
+                    else
+                    {
+                        //Promedio pares
+                        promedio = promedio + i;
+                        cantProm = cantProm + 1;
+                    }
+                }
+                //fin del for 
+
+                ////////////////////////////////////////////////////
+                // asignación de valores a los controles de salida
+                txtSumatoria.Enabled = true;
+                txtSumatoria.Text = suma.ToString();
+                promedio = promedio / cantProm;
+                txtPromedio.Enabled = true;
+                txtPromedio.Text = promedio.ToString();
+                ////////////////////////////////////////////////////
+                ///
+
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Ingreso un dato null, favor ejecutar nuevamente el programa", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtIngresar.Text = "";
+            txtPromedio.Text = "";
+            txtSumatoria.Text = "";
+            txtIngresar.Focus();
+            txtSumatoria.Enabled = false;
+            txtPromedio.Enabled = false;
+        }
+    }
+}
